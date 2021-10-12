72b5c92e4125a0e6300de980324d84765ab2fbc4
komi3844 <47776516+komi3844@users.noreply.github.com>
2021-10-12 16:28:15 +0300

    Add files via upload

---
+++ checkio/popular-words.py
@@ -0,0 +1,31 @@
+def popular_words(text: str, words: list) -> dict:
+    tex = ' '.join(text.lower().split())
+    x = tex.split()
+    slov = {}
+    for k_text in words:
+        slov[k_text] = x.count(k_text)
+    return slov
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0,
+        'near': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
